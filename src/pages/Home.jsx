9635a91563971de5259a7afdf6116c4670434595
IVAN3K2 <ivanasegning@gmail.com>
2025-12-24 09:21:18 +0100

    SEO : meta tags + optimisation Google

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,6 +18,14 @@ export default function Home() {
       <WorkProcess />
       <Projects />
       <CTA />
+      <section className="sr-only">
+        <h2>Agence digitale NEOpixel</h2>
+        <p>
+          NEOpixel est une agence digitale spécialisée en développement web et mobile,
+          création de sites internet, design graphique, montage vidéo et marketing digital.
+          Nous accompagnons les entreprises dans leur transformation numérique.
+        </p>
+      </section>
       <Footer />
       <BackToTop />
     </>
